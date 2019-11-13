df3cd89fd6e68508926303cfbb58ccbfe24c5e9a
Michael Trombola <mishatrombola@gmail.com>
2019-11-13 17:14:47 +0200

    add kotlin to project

--- JavaComandLine/src/com/company/Main.java
+++ JavaComandLine/src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import com.company.tasks._1_two_sum;
+
+import static com.company.tasks._1_two_sum.output;
+
 public class Main {
 
     public static void main(String[] args) {
 //         write your code here
-//        System.out.println(output(_1_two_sum.twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(output(_1_two_sum.twoSum(new int[]{2, 7, 11, 15}, 9)));
 
     }
 
---
+++ JavaComandLine/src/com/company/Main.kt
@@ -0,0 +1,12 @@
+package com.company
+
+fun main(args: Array<String>) {
+
+//    val a: MutableList<String> = MutableList(0, enumValueOf("test"));
+//    a.filter {
+//        it == "1"
+//    }.map {
+//        println(it)
+//    }
+    println("Test")
+}
